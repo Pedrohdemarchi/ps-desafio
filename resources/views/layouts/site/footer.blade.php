aca065b255da114c8e1dcb56af3c749b8f2fe2b1
Pedro De Marchi <pphhssddmm@gmail.com>
2023-03-11 12:40:00 -0300

    nav feito

--- resources/views/layouts/site/footer.blade.php
+++ resources/views/layouts/site/footer.blade.php
@@ -1,15 +1,18 @@
 <footer id="site-footer">
     <div id="footer-container">
         <div id="logo-footer">
-            <img src="{{asset("site/img/logo2.png")}}" alt="Logo 2" onclick="openPage=('http://127.0.0.1:8000/siteIndex')">
+            <a href="{{ route('siteIndex') }}">
+                <img src="{{asset("site/img/logo2.png")}}" alt="Logo 2">
+            </a>
+
         </div>
         <div id="text-footer">
             Pedro De Marchi, 2023. Todos os direitos reservados.
         </div>
         <div id="social-media">
-            <img src="{{asset("site/img/fb.png")}}" alt="Logo Facebook" onclick="openPage=('https://www.facebook.com/pedro.santosdemarchi?mibextid=ZbWKwL')">
-            <img src="{{asset("site/img/tt.png")}}" alt="Logo Twitter" onclick="openPage=('https://twitter.com/PedrohDeMarchi8?t=zdVUqsS2C5fu5IT2QgZq1g&s=09')">
-            <img src="{{asset("site/img/ig.png")}}" alt="Logo Instagram" onclick="openPage=('https://instagram.com/pedrohdemarchi8?igshid=ZDdkNTZiNTM=')">
+            <img src="{{asset("site/img/fb.png")}}" alt="Logo Facebook" onclick="openPage('https://www.facebook.com/pedro.santosdemarchi?mibextid=ZbWKwL')">
+            <img src="{{asset("site/img/tt.png")}}" alt="Logo Twitter" onclick="openPage('https://twitter.com/PedrohDeMarchi8?t=zdVUqsS2C5fu5IT2QgZq1g&s=09')">
+            <img src="{{asset("site/img/ig.png")}}" alt="Logo Instagram" onclick="openPage('https://instagram.com/pedrohdemarchi8?igshid=ZDdkNTZiNTM=')">
         </div>
     </div>
 </footer>
--- resources/views/layouts/site/header.blade.php
+++ resources/views/layouts/site/header.blade.php
@@ -1,8 +1,13 @@
 <header id="site-header">
     <div id="header-logo">
-        <a href="{{ route('siteIndex') }}">
             <img src="{{asset("site/img/logo1.png")}}" alt="Logo 1">
+    </div>
+    <div class="nav">
+        <h3 class="adm" onclick="openPage('http://127.0.0.1:8000')">Sou Administrador</h3>
+        <a href="{{ route('siteIndex') }}">
+            <h3>Home</h3>
         </a>
+
     </div>
     <div id="header-search">
         <form class="search"action="{{ route('productFind')}}">
