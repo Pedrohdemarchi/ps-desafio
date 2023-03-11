3ed33f05ede76feadc610b2abdddc4016e47db37
Pedro De Marchi <pphhssddmm@gmail.com>
2023-03-11 08:15:24 -0300

    Slider feito

--- public/site/js/script.js
+++ public/site/js/script.js
@@ -1,3 +1,19 @@
+let count = 1;
+document.getElementById("radio1").checked = true ;
+
+setInterval(function(){
+    nextImage();
+},5000)
+
+function nextImage(){
+    count++;
+    if(count>3){
+        count=1;
+    }
+
+    document.getElementById("radio"+count).checked = true ;
+}
+
 function openPage(link){
 window.open(link, "_blank").focus();
 }
@@ -15,12 +31,8 @@ for(card of cards){
                 document.getElementById(id).addEventListener('click', () => {
                     e.classList.remove('abrir')
                 })
-
             }
-
         });
-
-
     })
 }
 
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -5,8 +5,40 @@ Pedrão Hamburgueria
 @endsection
 
 @section('content')
-    <section class="produto-container">
-        <div class="produto-content">
+    <section class="site-container">
+        <div class="body-slide">
+            <div class="slider">
+                <div class="slides">
+                    <input type="radio" name="radio-btn" id="radio1">
+                    <input type="radio" name="radio-btn" id="radio2">
+                    <input type="radio" name="radio-btn" id="radio3">
+
+                    <div class="slide first">
+                        <img src="{{asset("site/img/S1.png")}}" alt="imagem 1">
+                    </div>
+                    <div class="slide">
+                        <img src="{{asset("site/img/S2.png")}}" alt="imagem 2">
+                    </div>
+                    <div class="slide">
+                        <img src="{{asset("site/img/S3.png")}}" alt="imagem 3">
+                    </div>
+
+                    <div class="navegation-auto">
+                        <div class="auto-btn1"></div>
+                        <div class="auto-btn2"></div>
+                        <div class="auto-btn3"></div>
+                    </div>
+
+                </div>
+
+                <div class="manual-navegation">
+                    <label for="radio1" class="manual-btn"></label>
+                    <label for="radio2" class="manual-btn"></label>
+                    <label for="radio3" class="manual-btn"></label>
+                </div>
+            </div>
+        </div>
+        <div class="product-content">
             <div class="title-product">
                 <h1>Guarnições da casa:</h1>
                 <form class="select" action="{{ route('productFilter')}}">
@@ -27,25 +59,24 @@ Pedrão Hamburgueria
                 @isset($produtos)
                     @if (count($produtos))
                         @foreach ($produtos as $produto)
-                                <div class="{{ $produto['quantidade'] == 0 ? 'each-product0' : 'each-product'}}" id="{{ $produto->id }}">
-                                        <div class="image-container">
-                                            <img src="/storage/{{ $produto['image'] }}">
-                                        </div>
-                                    <div class="info-product">
-                                        <h1>{{ $produto['name'] }}</h1>
-                                        <p>{{ $produto->categoria->name}}</p>
-                                        <p>${{ $produto['preco'] }}</p>
-                                        <p>{{ $produto['quantidade'] }} unidades</p>
-                                    </div>
+                            <div class="{{ $produto['quantidade'] == 0 ? 'each-product0' : 'each-product'}}" id="{{ $produto->id }}">
+                                <div class="image-container">
+                                    <img src="/storage/{{ $produto['image'] }}">
                                 </div>
-                                <div class="janela-modal" id="janela-modal{{ $produto->id }}">
-                                    <div class="modal">
-                                        <button class="fechar" id="fechar{{ $produto->id }}">X</button>
-                                        <h1>Descrição do {{ $produto['name'] }}:</h1>
-                                        <br>
-                                        <p>{{ $produto->descricao }}</p>
-                                    </div>
+                                <div class="info-product">
+                                    <h1>{{ $produto['name'] }}</h1>
+                                    <p>{{ $produto->categoria->name}}</p>
+                                    <p>${{ $produto['preco'] }}</p>                                        <p>{{ $produto['quantidade'] }} unidades</p>
                                 </div>
+                             </div>
+                            <div class="janela-modal" id="janela-modal{{ $produto->id }}">
+                                <div class="modal">
+                                    <button class="fechar" id="fechar{{ $produto->id }}">X</button>
+                                    <h1>Descrição do {{ $produto['name'] }}:</h1>
+                                    <br>
+                                    <p>{{ $produto->descricao }}</p>
+                                </div>
+                            </div>
                         @endforeach
                     @else
                         <p class="no-produto">Sem produtos</p>
