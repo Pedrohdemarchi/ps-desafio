aa20fab0a4d8051e89560589c588ecf88ddc98db
Pedrohdemarchi <pphhssddmm@gmail.com>
2026-01-31 21:01:54 -0300

    🎨 style(footer): add responsive footer styling

--- resources/views/layouts/site/footer.blade.php
+++ resources/views/layouts/site/footer.blade.php
@@ -1,1 +1,18 @@
-<h2>Maior do Rio</h2>
+<footer id="site-footer">
+    <div id="footer-container">
+        <div id="right-block">
+            <div id="logo-slogan">
+                <div id="btn1" onclick="openPage('http://localhost:8000/Kook')">LOGO</div>
+                <div id="slogan-footer">Projeto, reconhecimento, expansão.<br>Construindo legados.</div>
+            </div>
+            <div id="no-copy">Copyright 2026 KOOK St, Inc. Terms & Privacy</div>
+        </div>
+        <div id="left-block">
+            <div id="btn1">Descubra</div>
+            <div id="btn2">Legado</div>
+            <div id="btn2">Cases</div>
+            <div id="btn2">Contato</div>
+            <div id="btn2">Duvidas Frequentes</div>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/site/site.blade.php
+++ resources/views/layouts/site/site.blade.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titulo')</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="{{asset('site/css/site.css')}}">
 </head>
 <body>
     @include('layouts.site.header')
@@ -11,5 +18,7 @@
     @yield('conteudo')
 
     @include('layouts.site.footer')
+
+    <script src="{{asset('site/js/script.js')}}"></script>
 </body>
 </html>
