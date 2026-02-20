e90256256e5ddeff08ca9afd9c31ac7b92c1692f
Pedrohdemarchi <pphhssddmm@gmail.com>
2026-02-20 01:31:42 -0300

    🎨 style(body): add responsive body styling

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,9 +1,33 @@
 @extends('layouts.site.site')
 
 @section('titulo')
-    Kook St
+    Kook Studio
 @endsection
 
 @section('conteudo')
-    <h1>Vasco da gama</h1>
+    <section class="showcase-container">
+        <div class="showcases">
+            @isset($showcases)
+                @if(count($showcases))
+                    @foreach ($showcases as $showcase)
+                        <div class="card-showcase">
+                            <div class="left-card">
+                                <img src="{{ asset('storage/' . $showcase['logo_showcase']) }}" class="logo-showcase">
+                                <div class="block-left">
+                                    <h2 class="title-showcase">{{$showcase['title_showcase']}}</h2>
+                                    <div class="text-showcase">{{$showcase['description_showcase']}}</div>
+                                    <div class="more">read more -></div>
+                                </div>
+                            </div>
+                            <div class="right-card">
+                                <img src="{{ asset('storage/' . $showcase['image_showcase']) }}" class="image-showcase">
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="no-showcase">Sem nenhum serviço cadastrado.</div>
+                @endif
+            @endisset
+        </div>
+    </section>
 @endsection
